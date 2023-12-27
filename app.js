85f1e39bb12513889f136cbade2caefbdd6f7106
PunitVerma <punit.verma@cubastion.com>
2023-12-27 12:52:47 +0530

    adding server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('./config/db.js');
+const app = express();
+
+const port = 3000;
+app.use(express.json());
+
+app.get('', (req, res) => {
+    res.send('kikiki')
+})
+
+app.listen(port, () => {
+    console.log('server is up and running on ' + port)
+})
